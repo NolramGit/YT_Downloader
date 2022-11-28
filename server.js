43a8a453ae3b7ed85eb8ed8aa97e5c1680d75680
NolramGit <nolrammado@gmail.com>
2022-11-28 16:23:19 -0500

    upload files

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, "views/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 })
 app.get("/service", (req, res) =>{
     res.send("Hola desde server!")
