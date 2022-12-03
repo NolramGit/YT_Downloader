3667e735c3d333c21897e82767480e194d42765b
NolramGit <nolrammado@gmail.com>
2022-12-02 19:06:40 -0500

    uplod files

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { stdout } from "./lib/dl.mjs";
+import { ytDownload } from "./lib/dl.mjs";
 import os from "os";
 import path from 'path';
 import express from "express";
@@ -6,13 +6,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-console.log(stdout.stdout)
+
+let data = await ytDownload()
 
 app.get("/", (req, res) =>{
     res.sendFile(path.resolve("./"));
 })
 app.get("/service", (req, res) =>{
-    res.send(`Hola desde server!, ${os.platform()}`)
+    //res.send(`Hola desde server!, ${os.platform()}`)
+    res.send(`${data.title}`)
+    
 })
 
 app.listen(port, ()=>{
